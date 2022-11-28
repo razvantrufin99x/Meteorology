9f0bb052dd03a9de05eafb1dda8f10dd80da1f19
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-11-28 02:15:07 +0200

    Add project files.

---
+++ Meteorology/MeteoDatasRecord.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meteorology
+{
+    public class MeteoDatasRecord
+    {
+        public string UniqueRecordNumber = "0000-00-00-00-00-00-000-00000000000000000000000";
+        public DateTime DateAndTime = new DateTime();
+        public float TemperatureCelsiusDegrees = 0.0f;
+        public string MeteoConditions = "";
+        public string MeteoWarningMessage = "";
+        public string Latitude = "";
+        public string Longitude = "";
+        public string Country = "";
+        public string Language = "";
+        public bool InPast = false;
+
+        public MeteoDatasRecord() { }
+        public MeteoDatasRecord(string uniqueRN, DateTime dt, float TCS, string mc, string mwm, string lat, string lon, string cn, string la, bool p)
+        {
+            UniqueRecordNumber = uniqueRN;
+            DateAndTime = dt;
+            TemperatureCelsiusDegrees = TCS;
+            MeteoConditions = mc;
+            MeteoWarningMessage = mwm;
+            Latitude = lat;
+            Longitude = lon;
+            Country = cn;
+            Language = la;
+            InPast = p;
+        }
+
+    }
+}
---
+++ Meteorology/TabelsOfMeteoValues.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Meteorology
+{
+    public class TabelsOfMeteoValues
+    {
+        public DateTime CurrentDateTime { get; set; }
+        public List <MeteoDatasRecord> TheTableOfMeteoValues = new List<MeteoDatasRecord> ();
+        public List <string > MeteoPrevisions = new List<string> ();
+
+        public TabelsOfMeteoValues() { }
+        public TabelsOfMeteoValues(MeteoDatasRecord mdr) { 
+            TheTableOfMeteoValues.Add (mdr);
+        }
+
+        public void MakePrevisionOfMeteoConditions() {
+            for (int i = 0; i < TheTableOfMeteoValues.Count; i++)
+            {
+                if (TheTableOfMeteoValues[i].InPast == false)
+                { 
+                    ///make prediction
+                    /////and add a new future message in MeteoPrevisions
+                }
+            }
+        }
+    }
+}
